04b201471b01b99967a9038a6a110e13a2294516
irfannm21 <irfannurmuhammad21@gmail.com>
2023-01-05 23:42:35 +0700

    feature pembayaran

--- database/seeds/SimpananWajibTableSeeder.php
+++ database/seeds/SimpananWajibTableSeeder.php
@@ -12,12 +12,14 @@ class SimpananWajibTableSeeder extends Seeder
      */
     public function run()
     {
-        $anggota = Anggota::find(2);
+        $anggota = Anggota::find(3);
 
         $simpanan = new SimpananWajib;
-        $simpanan->tanggal = "2023-02-01";
+        $simpanan->tanggal = "2023-12-01";
         $simpanan->jumlah_simpanan = "150000";
 
         $anggota->simpanan_wajibs()->save($simpanan);
+
+
     }
 }
